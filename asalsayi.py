9b8b69da67765b3fa9535ae01719c43809389607
Berkay <yasarkacar3420@gmail.com>
2025-07-03 16:37:10 +0300

    asalsayi.py
    # ler sayesinde daha anlaşılabilir bir kod

--- asalsayi.py
+++ asalsayi.py
@@ -1,12 +1,12 @@
-Sayi = int(input("Bir sayı giriniz: "))
+Sayi = int(input("Bir sayı giriniz: ")) #Kullacının sayıyı girebilmesi için olan yer
 
-prime = True
-for a in range(2, Sayi):
-  if Sayi %a == 0:
-    prime = False
-    break
+prime = True         # gireceğimiz sayının asal sayı olduğunu varsayıyoruz
+for a in range(2, Sayi):     # Bu iki satırda kullanıcının girdiği sayıya kadar bütün sayıları kullanıcının sayısına bölüyoruz
+  if Sayi %a == 0:           # (2, sayi) kısmında 2 den başlattım çünkü 1 den başlatsaydım her sayı 1 e bölünebildiği için olmazdı
+    prime = False         # bu satırda eğer sayımız sayımıza kadar olan sayılardan birine bölünebiliyorsa bu sayının asal sayı olmadığını bildiriyoruz
+    break                # eğer sayımız sayımıza kadar olan sayılardan birine bölünebiliyorsa if komudunun içine giriyor ve break komudu sayesinde bitiriyoruz
 
-if prime == True:
+if prime == True:              #bu komutla bu sayı if komuduna takılmamış ve prime == True ise bize sayının asal olduğunu bildir diyoruz
    print(f"{Sayi} sayisi asal")
-else:
-   print(f"{Sayi} sayisi asal değildir")
+else:   
+   print(f"{Sayi} sayisi asal değildir")   #burada ise eğer prime == True den farklı bir şey yani prime == False ise sayımızın bize asal olmadığını bildir diyoruz ve kodumuz bitiyor.
